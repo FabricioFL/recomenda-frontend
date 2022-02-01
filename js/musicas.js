548338b5bb6098507ab24d1d6a312736b53b460c
Fabricio Freitas Lima <fabriciofl.dev@gmail.com>
2022-02-01 00:53:14 -0300

    correções

---
+++ js/musicas.js
@@ -0,0 +1,114 @@
+rock = [
+"https://www.youtube.com/watch?v=eVTXPUF4Oz4&ab_channel=LinkinPark",
+"https://www.youtube.com/watch?v=aCyGvGEtOwc&ab_channel=Paramore",
+"https://www.youtube.com/watch?v=_ikArCV_zD0&ab_channel=MotherMother-Topic",
+"https://www.youtube.com/watch?v=lv1K0H_4mfw&ab_channel=MotherMother-Topic",
+"https://www.youtube.com/watch?v=v8ZAVruZ63M&ab_channel=MotherMother",
+"https://www.youtube.com/watch?v=fFt0s7crDfo&ab_channel=MotherMotherVEVO",
+"https://www.youtube.com/watch?v=RRKJiM9Njr8&ab_channel=MyChemicalRomance",
+"https://www.youtube.com/watch?v=-k9qDxyxS3s&ab_channel=BMTHOfficialVEVO",
+"https://www.youtube.com/watch?v=QJJYpsA5tv8&ab_channel=BMTHOfficialVEVO",
+"https://www.youtube.com/watch?v=810zk7nqMLI&list=PLBFFB156E473F8A94&index=2&ab_channel=ff8addict1",
+"https://www.youtube.com/watch?v=RB-RcX5DS5A&list=PL7JYADiEFo7mOe_dJ0m_XR4lrI_KgI7Rq&ab_channel=Coldplay",
+"https://www.youtube.com/watch?v=VUb1p8fm7Ag&list=PLxa1YYCVnUZXxTEfbAnlLtMb2rR0j8A4O&ab_channel=Nightwish",
+"https://www.youtube.com/watch?v=8SbUC-UaAxE&list=PLBB13F295B0C02A30&index=1&ab_channel=GunsNRosesVEVO",
+"https://www.youtube.com/watch?v=xSQD_IKZzIQ&list=PL6wb5PaEJRC15qCeN4Q0bCp-mNwv8ZCe8&index=1&ab_channel=Skindred-Topic"
+]
+
+electroswing = [
+    "https://www.youtube.com/watch?v=7oF3Zloa258",
+    "https://www.youtube.com/watch?v=JWp4JXc_0uY&ab_channel=JamieBerry-Topic",
+    "https://www.youtube.com/watch?v=zwUyJCZlovc&ab_channel=JamieBerry-Topic",
+    "https://www.youtube.com/watch?v=9bACyurDLlg&ab_channel=LittleViolet-Topic",
+    "https://www.youtube.com/watch?v=ksOmYUKy6Dg&ab_channel=LittleViolet-Topic",
+    "https://www.youtube.com/watch?v=xQXOPA7nZpA&ab_channel=CaroEmerald-Topic",
+    "https://www.youtube.com/watch?v=uYngyQ6TdXY&ab_channel=caroemerald",
+    "https://www.youtube.com/watch?v=aGB9HiI79Ho&ab_channel=caroemerald",
+    "https://www.youtube.com/watch?v=EUV2GPas4aY&ab_channel=caroemerald",
+    "https://www.youtube.com/watch?v=Yjl890_qwf4&ab_channel=caroemerald",
+    "https://www.youtube.com/watch?v=fiDHjKFcL9g&ab_channel=FreshlySqueezed",
+    "https://www.youtube.com/watch?v=S-3R5bA1G_A&ab_channel=ImmortalSwings",
+    "https://www.youtube.com/watch?v=kx--qcNpxEQ&ab_channel=ImmortalSwings",
+    "https://www.youtube.com/watch?v=dQP6dye4XsE&ab_channel=Swingrowers-Topic",
+    "https://www.youtube.com/watch?v=iPp8gHbdb9k&ab_channel=ImmortalSwings",
+    "https://www.youtube.com/watch?v=ipvEIZMMILA&ab_channel=AronChupaVEVO",
+    "https://www.youtube.com/watch?v=Bznxx12Ptl0&ab_channel=AronChupaVEVO",
+    "https://www.youtube.com/watch?v=FHccClTAdzc&ab_channel=CazzetteVEVO",
+    "https://www.youtube.com/watch?v=znrEGeYsq9s&ab_channel=ParovStelar",
+    "https://www.youtube.com/watch?v=MJhJJLdxyFQ&ab_channel=dunkelbuntrecords",
+    "https://www.youtube.com/watch?v=fPRLc6t0U5o&ab_channel=WolfgangLohr",
+    "https://www.youtube.com/watch?v=wZZ8V2AFUq0&ab_channel=ElectricSwingCircus",
+    "https://www.youtube.com/watch?v=dzhcil9ehbo&ab_channel=FreshlySqueezed",
+    "https://www.youtube.com/watch?v=TbsBEb1ZxWA&ab_channel=CaravanPalace",
+    "https://www.youtube.com/watch?v=9Z9MTSUrvQ8&ab_channel=OR3O",
+    "https://www.youtube.com/watch?v=AlYdp8P1s6c&ab_channel=OR3O",
+    "https://www.youtube.com/watch?v=_TTqjLv0PYI&ab_channel=OR3O",
+    "https://www.youtube.com/watch?v=wsFB9Z7DfFc&ab_channel=OR3O",
+    "https://www.youtube.com/watch?v=iV9fxNi46zU&ab_channel=SquigglyDigg",
+    "https://www.youtube.com/watch?v=5qx4wmsPOAI&ab_channel=OR3O",
+    "https://www.youtube.com/watch?v=MPmD6hFaomk&ab_channel=EDMBot"
+]
+
+pop = [
+    "https://www.youtube.com/watch?v=Ix4L9am2Kwk&ab_channel=dojacatVEVO",
+    "https://www.youtube.com/watch?v=nhwIdvXVDsE&ab_channel=DojaCat-Topic",
+    "https://www.youtube.com/watch?v=FA375RUlXsc&ab_channel=DojaCat-Topic",
+    "https://www.youtube.com/watch?v=XEJLuJyxLDE&ab_channel=dojacatVEVO",
+    "https://www.youtube.com/watch?v=N000qglmmY0&ab_channel=DuaLipa",
+    "https://www.youtube.com/watch?v=oygrmJFKYZY&ab_channel=DuaLipa",
+    "https://www.youtube.com/watch?v=L3wKzyIN1yk&ab_channel=RagnBoneManVEVO",
+    "https://www.youtube.com/watch?v=PUdyuKaGQd4&ab_channel=AliceMerton",
+    "https://www.youtube.com/watch?v=IO2aTF7ygPE&ab_channel=HasseneAyoub",
+    "https://www.youtube.com/watch?v=Rb6Scz-5YOs&ab_channel=BeneeMusicVEVO",
+    "https://www.youtube.com/watch?v=VHi1kGbWo9Q&ab_channel=MARINA-Topic",
+    "https://www.youtube.com/watch?v=0J3vgcE5i2o&ab_channel=NellyFurtadoVEVO",
+    "https://www.youtube.com/watch?v=6Hyf7CIf_K4&ab_channel=TristanMusic",
+    "https://www.youtube.com/watch?v=VOn6i2spFIY&ab_channel=MARINA-Topic",
+    "https://www.youtube.com/watch?v=xIF0Me8j0dg&ab_channel=GwenStefani-Topic",
+    "https://www.youtube.com/watch?v=d9rU1dwVGJw&ab_channel=7thHeaven",
+    "https://www.youtube.com/watch?v=YEbz2Qt3vec&ab_channel=BillieEilishVEVO",
+    "https://www.youtube.com/watch?v=4-NkTMyVq2c&ab_channel=DuaLipa-Topic",
+    "https://www.youtube.com/watch?v=djV11Xbc914&ab_channel=a-ha",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+    "https://www.youtube.com/watch?v=V5M2WZiAy6k&ab_channel=ImagineDragonsVEVO",
+    "https://www.youtube.com/watch?v=vzPl2nhgbEs&ab_channel=RareVibes",
+    "https://www.youtube.com/watch?v=kE0unyetlBw&ab_channel=AlexGaudino-Topic",
+    "https://www.youtube.com/watch?v=Zrd60LuRr_U&ab_channel=LuliiLIE",
+    "https://www.youtube.com/watch?v=1K3U8x2P4gM&ab_channel=EternityMusic",
+    "https://www.youtube.com/watch?v=y_fB0IMbq54&ab_channel=RiotGamesMusic",
+    "https://www.youtube.com/watch?v=1RiGNU34Q_c&ab_channel=Clarissa",
+    "https://www.youtube.com/watch?v=k1BneeJTDcU&ab_channel=boburnham",
+    "https://www.youtube.com/watch?v=PZ6rogD8Eds&ab_channel=Isabel",
+    "https://www.youtube.com/watch?v=lcg6wekmCRA&ab_channel=TrapNation",
+    "https://www.youtube.com/watch?v=BXB26PzV31k&ab_channel=MoriCalliopeCh.hololive-EN",
+    "https://www.youtube.com/watch?v=6Iz8qlWk4hY&ab_channel=Demondice-Topic",
+    "https://www.youtube.com/watch?v=IOrbP1OqNsg&ab_channel=ImagineDragons-Topic"
+]
+
+lofi = [
+    "https://www.youtube.com/watch?v=XE2IvHxdkDM&ab_channel=thebootlegboy",
+    "https://www.youtube.com/watch?v=_ITiwPMUzho&t=1213s&ab_channel=LofiGhostie",
+    "https://www.youtube.com/watch?v=NxSDNogkKX0&ab_channel=Dreamy",
+    "https://www.youtube.com/watch?v=yFOdkyaoUTg&ab_channel=Hitkend",
+    "https://www.youtube.com/watch?v=ZhstyJSNKME&ab_channel=CORTESCANHOTOS",
+]
+
+function rockAleatorio()
+{
+    window.open(rock[Math.floor(Math.random() * rock.length)], '_blank')
+}
+
+function electroswingAleatorio()
+{
+    window.open(electroswing[Math.floor(Math.random() * electroswing.length)], '_blank')
+}
+
+function popAleatorio()
+{
+    window.open(pop[Math.floor(Math.random() * pop.length)], '_blank')
+}
+
+function lofiAleatorio()
+{
+    window.open(lofi[Math.floor(Math.random() * lofi.length)], '_blank')
+}
